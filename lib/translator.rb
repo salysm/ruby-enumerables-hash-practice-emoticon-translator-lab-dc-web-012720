5f8a4d204bc427c3757ddb46ccffe79f53165e3f
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 16:46:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,22 +12,3 @@ def get_english_meaning
   # code goes here
 end
 
-test = {
-  :angel => ["O:)", ":("],
-  :angry => [">:(", ":)"]
-  }
-
-
-
-def translator(test)
-  puts "Please enter the emoticon that you'd like translate"
-  input = gets.chomp
-  input2 = gets.downcase.chomp
-  test.each do |emotion, emoticons|
-    if emoticons.first == input
-      puts "Here is the Japanese version of your emoticon:   #{emoticons.last}"
-    end
-  end
-end
-
-translator(test)
